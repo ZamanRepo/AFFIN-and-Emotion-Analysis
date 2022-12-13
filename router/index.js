3166dcd29f98e0639a1b2bbfd4575186e7148f95
neel2911 <neelrokr@gmail.com>
2022-12-12 20:07:13 -0500

    fix accessibility

--- router/index.js
+++ router/index.js
@@ -6,6 +6,9 @@ const aboutController = require("../controller/aboutController.js");
 router.get("/", indexController.get);
 router.post("/", indexController.post);
 
+router.get("/home", indexController.get);
+router.post("/home", indexController.post);
+
 router.get("/about", aboutController.get);
 
 module.exports = router;
